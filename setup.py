b33996eee681091438265d85c8f4c81e1378ba25
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-04-23 10:02:44 +0200

    Add numba-special to setup

--- setup.py
+++ setup.py
@@ -4,5 +4,10 @@ setup(
     name="gaussians",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=["numpy", "scipy", "numba",],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "numba",
+        "numba-scipy @ git+ssh://git@github.com/person142/numba-scipy.git#egg=numba-scipy-pin",
+    ],
 )
