a4d092f7680c6d4d07c643e79f140858acb077e6
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-04-24 20:59:45 +0200

    Compare with Joshua-code

--- tests/test_multipole_moment.py
+++ tests/test_multipole_moment.py
@@ -3,6 +3,8 @@ import numpy as np
 from gaussians import G1D, OD1D
 from gaussians.one_dim.multipole_moment import construct_multipole_moment_matrix
 
+from helpers.utils import overlap as j_overlap, E
+
 
 def overlap(G_i, G_j):
     O_ij = OD1D(G_i, G_j)
@@ -49,26 +51,30 @@ def construct_dipole_moment_matrix(gaussians):
     return d
 
 
-def test_overlap_dist():
-    gaussians = [G1D(0, 2, -4), G1D(0, 2, 4), G1D(1, 1, 0), G1D(2, 1, 0)]
-
-    for G_i in gaussians:
-        for G_j in gaussians:
-            O_ij = OD1D(G_i, G_j)
-            O_ji = OD1D(G_j, G_i)
-
-            for t in range(5):
-                print(t)
-                np.testing.assert_allclose(O_ij.E(t), O_ji.E(t))
-
-
 def test_overlap():
-    gaussians = [G1D(0, 2, -4), G1D(0, 2, 4), G1D(1, 1, 0), G1D(2, 1, 0)]
+    gaussians = [G1D(0, 1, 0.5), G1D(0, 0.5, 0), G1D(1, 1, 0), G1D(2, 1, 0)]
 
     s = construct_multipole_moment_matrix(0, 0, gaussians)
     s_2 = construct_overlap_matrix(gaussians)
 
+    x = np.linspace(-10, 10, 1001)
+
+    s_3 = np.zeros_like(s_2)
+    s_4 = np.zeros_like(s_2)
+
+    for i, G_i in enumerate(gaussians):
+        for j, G_j in enumerate(gaussians):
+            s_3[i, j] = G_i.norm * G_j.norm * np.trapz(G_i(x) * G_j(x), x=x)
+            s_4[i, j] = (
+                G_i.norm
+                * G_j.norm
+                * j_overlap(G_i.a, G_i.i, G_i.A, G_j.a, G_j.i, G_j.A)
+            )
+
     np.testing.assert_allclose(s, s_2)
+    np.testing.assert_allclose(s_3, s_4, atol=1e-12)
+    np.testing.assert_allclose(s, s_4)
+    np.testing.assert_allclose(s, s_3, atol=1e-12)
     np.testing.assert_allclose(s_2, s_2.T)
     np.testing.assert_allclose(s, s.T)
 
@@ -80,3 +86,4 @@ def test_dipole_moment():
     d_2 = construct_dipole_moment_matrix(gaussians)
 
     np.testing.assert_allclose(d, d_2)
+    np.testing.assert_allclose(d, d.T)
