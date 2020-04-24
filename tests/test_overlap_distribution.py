eeec28f080d6dd8386d140e8cb2ac369bb5cabe9
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-04-24 20:59:30 +0200

    Use Joshua-code from utils

--- tests/test_overlap_distribution.py
+++ tests/test_overlap_distribution.py
@@ -2,42 +2,7 @@ import numpy as np
 
 from gaussians import G1D, OD1D
 
-
-def E(i, j, t, Qx, a, b):
-    """Code from Joshua Goings:
-    https://joshuagoings.com/2017/04/28/integrals/
-
-    Recursive definition of Hermite Gaussian coefficients.
-    Returns a float.
-    a: orbital exponent on Gaussian 'a' (e.g. alpha in the text)
-    b: orbital exponent on Gaussian 'b' (e.g. beta in the text)
-    i,j: orbital angular momentum number on Gaussian 'a' and 'b'
-    t: number nodes in Hermite (depends on type of integral,
-       e.g. always zero for overlap integrals)
-    Qx: distance between origins of Gaussian 'a' and 'b'
-    """
-    p = a + b
-    q = a * b / p
-    if (t < 0) or (t > (i + j)):
-        # out of bounds for t
-        return 0.0
-    elif i == j == t == 0:
-        # base case
-        return np.exp(-q * Qx * Qx)  # K_AB
-    elif j == 0:
-        # decrement index i
-        return (
-            (1 / (2 * p)) * E(i - 1, j, t - 1, Qx, a, b)
-            - (q * Qx / a) * E(i - 1, j, t, Qx, a, b)
-            + (t + 1) * E(i - 1, j, t + 1, Qx, a, b)
-        )
-    else:
-        # decrement index j
-        return (
-            (1 / (2 * p)) * E(i, j - 1, t - 1, Qx, a, b)
-            + (q * Qx / b) * E(i, j - 1, t, Qx, a, b)
-            + (t + 1) * E(i, j - 1, t + 1, Qx, a, b)
-        )
+from helpers.utils import E
 
 
 def test_hermite_expansion_symmetry():
@@ -68,7 +33,6 @@ def test_hermite_expansion_symmetry():
             np.testing.assert_allclose(O_ij.K_AB, O_ji.K_AB)
 
             for t in range(max(angular_mom)):
-                print(t)
                 np.testing.assert_allclose(
                     E(O_ij.i, O_ij.j, t, O_ij.X_AB, O_ij.G_i.a, O_ij.G_j.a),
                     O_ij.E(t),
