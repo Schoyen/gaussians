846c8ccd93cb67d69687baa5129a3ddc3c9188d6
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-04-30 19:04:17 +0200

    Compare results from Hartree-Fock
        We get the same energy for both the Gaussian basis and the HO-basis.
        However, the plot of the single-particle states still differ.

--- tests/test_two_dim_coulomb_elements.py
+++ tests/test_two_dim_coulomb_elements.py
@@ -1,7 +1,12 @@
 import numpy as np
 import scipy.linalg
 
-from quantum_systems import TwoDimensionalHarmonicOscillator
+from quantum_systems import (
+    TwoDimensionalHarmonicOscillator,
+    BasisSet,
+    GeneralOrbitalSystem,
+)
+from hartree_fock import GHF
 
 from gaussians import G2D
 from gaussians.two_dim import (
@@ -26,7 +31,15 @@ def test_two_dim_ho():
 
     gaussians = [G2D((i, j), omega / 2) for i in range(l) for j in range(l)]
 
-    tdho = TwoDimensionalHarmonicOscillator(16, 5, 201, omega=omega)
+    tdho = GeneralOrbitalSystem(
+        2, TwoDimensionalHarmonicOscillator(l ** 2, 5, 201, omega=omega)
+    )
+
+    ghf_ho = GHF(tdho, verbose=True)
+    ghf_ho.compute_ground_state()
+
+    X = tdho._basis_set.R * np.cos(tdho._basis_set.T)
+    Y = tdho._basis_set.R * np.sin(tdho._basis_set.T)
 
     t = construct_kinetic_matrix_elements(gaussians)
     v = (
@@ -45,19 +58,34 @@ def test_two_dim_ho():
     s = construct_overlap_matrix_elements(gaussians)
     u = construct_coulomb_matrix_elements(gaussians)
 
-    epsilon_2, C = scipy.linalg.eigh(h, s)
+    gos = BasisSet(len(gaussians), dim=2)
+    gos.h = h
+    gos.s = s
+    gos.u = u
 
-    n = 4
+    spf = np.asarray([gauss(X, Y, with_norm=True) for gauss in gaussians])
+    gos.spf = spf
 
-    X = tdho.R * np.cos(tdho.T)
-    Y = tdho.R * np.sin(tdho.T)
+    gos = GeneralOrbitalSystem(2, gos)
 
-    epsilon = np.diag(tdho.h)
+    ghf_gauss = GHF(gos, verbose=True)
+    ghf_gauss.compute_ground_state()
 
-    np.testing.assert_allclose(epsilon[:n], epsilon_2[:n], atol=1e-12)
-    h_trans = tdho.transform_one_body_elements(h, C, np)
-    np.testing.assert_allclose(tdho.h[:n, :n], h_trans[:n, :n], atol=1e-12)
+    assert abs(ghf_ho.compute_energy() - ghf_gauss.compute_energy()) < 1e-12
 
-    u_trans = tdho.transform_two_body_elements(u, C, np)
+    # import matplotlib.pyplot as plt
 
-    np.testing.assert_allclose(tdho.u[:n, :n, :n, :n], u_trans[:n, :n, :n, :n])
+    # for i in range(0, tdho.l, 2):
+    #     plt.figure()
+    #     plt.title(f"Grid: {i}")
+    #     plt.contourf(X, Y, np.abs(tdho.spf[i]) ** 2)
+
+    # ghf_ho.change_basis()
+    # ghf_gauss.change_basis()
+
+    # for i in range(0, tdho.l, 2):
+    #     plt.figure()
+    #     plt.title(f"Analytic {i}")
+    #     plt.contourf(X, Y, np.abs(gos.spf[i]) ** 2)
+
+    # plt.show()
