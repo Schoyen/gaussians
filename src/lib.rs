dbd7f0ab15564cba449533c30420efaae9b6c4b3
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-05-11 16:15:40 +0200

    Add ndarray and numpy to rust
        We also add a temporary test of transitioning from Python with numpy
        to rust with ndarray.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use numpy::PyArrayDyn;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
@@ -6,9 +7,18 @@ fn ret_str() -> PyResult<String> {
     Ok("Hello, rusty world!".to_string())
 }
 
+#[pyfunction]
+fn mul_arr(_py: Python, x: &PyArrayDyn<f64>) -> PyResult<()> {
+    let mut x = x.as_array_mut();
+    x.map_inplace(|y| *y *= 2.0);
+
+    Ok(())
+}
+
 #[pymodule(gaussian_lib)]
 fn init_mod(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(ret_str))?;
+    m.add_wrapped(wrap_pyfunction!(mul_arr))?;
 
     Ok(())
 }
---
+++ tests/test_rust_inplace.py
@@ -0,0 +1,11 @@
+import numpy as np
+from gaussians.gaussian_lib import mul_arr
+
+
+def test_mut_arr():
+    a = np.random.random((100, 57))
+    b = a.copy()
+
+    mul_arr(a)
+
+    np.testing.assert_allclose(2 * b, a)
