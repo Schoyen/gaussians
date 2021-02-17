46a06f1589056eef57be108957de6bcda09cfedd
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2021-02-17 08:34:33 +0100

    Activate test and use smaller array

--- tests/test_rust_inplace.py
+++ tests/test_rust_inplace.py
@@ -1,13 +1,10 @@
-import pytest
-
 import numpy as np
 
 
-@pytest.mark.skip
 def test_mut_arr():
     from gaussians.gaussian_lib import mul_arr
 
-    a = np.random.random((100, 57))
+    a = np.random.random((5, 4))
     b = a.copy()
 
     mul_arr(a)
