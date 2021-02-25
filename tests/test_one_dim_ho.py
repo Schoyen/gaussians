6ee50d5c6353f5bb09fd86c95463500e8340db23
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2021-02-25 15:20:23 +0100

    Compare one-dim HO in Rust and Python

--- tests/test_one_dim_ho.py
+++ tests/test_one_dim_ho.py
@@ -5,9 +5,13 @@ import scipy.linalg
 
 from gaussians import G1D
 from gaussians.one_dim import (
+    construct_overlap_matrix_elements,
+    construct_kinetic_matrix_elements,
     construct_differential_matrix_elements,
     construct_multipole_moment_matrix_elements,
 )
+
+import gaussians.one_dim_lib as odl
 
 
 @pytest.fixture
@@ -44,3 +48,43 @@ def test_eigenenergies(odho_hamiltonian):
     # We only test the first 10 eigenenergies. Higher order will be worse.
     for i in range(10):
         np.testing.assert_allclose(energies[i], eps(i), atol=1e-7)
+
+
+def test_rust_one_dim_lib():
+    import time
+
+    omega = 0.5
+    l = 20
+
+    gaussians = [G1D(i, omega / np.sqrt(2), 0) for i in range(l)]
+
+    t_0 = time.time()
+    t = construct_kinetic_matrix_elements(gaussians)
+    v = (
+        0.5
+        * omega ** 2
+        * construct_multipole_moment_matrix_elements(2, 0, gaussians)
+    )
+    s = construct_overlap_matrix_elements(gaussians)
+    t_1 = time.time()
+    print(f"Time Python: {t_1 - t_0} sec")
+    h = t + v
+
+    g_params = [g.get_params() for g in gaussians]
+
+    t_0 = time.time()
+    t_r = odl.construct_kinetic_operator_matrix_elements(g_params)
+    v_r = (
+        0.5
+        * omega ** 2
+        * odl.construct_multipole_moment_matrix_elements(2, 0, g_params)
+    )
+    s_r = odl.construct_overlap_matrix_elements(g_params)
+    t_1 = time.time()
+    print(f"Time Rust: {t_1 - t_0} sec")
+    h_r = t_r + v_r
+
+    np.testing.assert_allclose(t, t_r)
+    np.testing.assert_allclose(v, v_r)
+    np.testing.assert_allclose(s, s_r)
+    np.testing.assert_allclose(h, h_r)
