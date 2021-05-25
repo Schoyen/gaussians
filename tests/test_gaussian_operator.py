15daec3c33b5d4dcac8f3e013e19f3d38369e30d
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2021-05-25 12:16:29 +0200

    Fix tests and add symmetry test

--- tests/test_gaussian_operator.py
+++ tests/test_gaussian_operator.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from gaussians import G1D
 from gaussians.one_dim import construct_gaussian_operator_matrix_elements
+from gaussians.one_dim.gaussian_operator import G
 
 
 def test_simplest_case():
@@ -24,12 +25,31 @@ def test_simplest_case():
     np.testing.assert_allclose(g_e, g_test)
 
 
-@__import__("pytest").mark.skip()
+def test_symmetry_of_integrals():
+    gaussians = [G1D(0, 1), G1D(1, 1.5), G1D(2, 0.7), G1D(0, 0.5)]
+    wells = [G1D(0, 0.3), G1D(1, 1.2), G1D(2, 1.1)]
+
+    for well in wells:
+
+        for i, g_i in enumerate(gaussians):
+            for j, g_j in enumerate(gaussians):
+                val = g_i.norm * g_j.norm * G(well, g_i, g_j)
+                val_2 = g_i.norm * g_j.norm * G(well, g_j, g_i)
+                val_3 = g_i.norm * g_j.norm * G(g_i, g_j, well)
+                val_4 = g_i.norm * g_j.norm * G(g_j, g_i, well)
+                val_5 = g_i.norm * g_j.norm * G(g_j, well, g_i)
+                val_6 = g_i.norm * g_j.norm * G(g_i, well, g_j)
+
+                assert abs(val - val_2) < 1e-12
+                assert abs(val - val_3) < 1e-12
+                assert abs(val - val_4) < 1e-12
+                assert abs(val - val_5) < 1e-12
+                assert abs(val - val_6) < 1e-12
+
+
 def test_origin_center():
     gaussians = [G1D(0, 1), G1D(1, 1.5), G1D(2, 0.7), G1D(0, 0.5)]
     wells = [G1D(0, 0.3), G1D(1, 1.2), G1D(2, 1.1)]
-    gaussians = [G1D(2, 1)]
-    wells = [G1D(0, 0.3)]
     res_func = [
         lambda a: -np.sqrt(np.pi / a),
         lambda a: 0,
@@ -50,11 +70,6 @@ def test_origin_center():
                 exp_sum = g_i.a + g_j.a + well.a
 
                 g_test[i, j] = g_i.norm * g_j.norm * res_func[ang_sum](exp_sum)
-                print(i, j, g_i.i, g_j.i, well.i, ang_sum, exp_sum)
-                # print(res_func[ang_sum](1))
-                # np.testing.assert_allclose(g_e[i, j], g_test[i, j])
 
-        # print(well.i)
         np.testing.assert_allclose(g_e, g_test)
         np.testing.assert_allclose(g_e, g_e.T)
-        wat
