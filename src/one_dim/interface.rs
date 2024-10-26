863cf5c94a7663b9b028ef71d64e868126754a04
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2024-10-26 22:15:14 +0200

    Fix breaking changes in updated PyO3 libraries

--- src/one_dim/interface.rs
+++ src/one_dim/interface.rs
@@ -2,11 +2,11 @@ use pyo3::prelude::*;
 use pyo3::types::PySequence;
 use pyo3::wrap_pyfunction;
 
-use numpy::{PyArray2, ToPyArray};
+use numpy::{IntoPyArray, PyArray2};
 
 use gs_lib::one_dim::G1D;
 
-fn set_up_g1d_vec(g1d_params: &PySequence) -> Vec<G1D> {
+fn set_up_g1d_vec<'py>(g1d_params: &Bound<'py, PySequence>) -> Vec<G1D> {
     let g1d_param_vec =
         g1d_params.extract::<Vec<(u32, f64, f64, char)>>().unwrap();
 
@@ -17,79 +17,79 @@ fn set_up_g1d_vec(g1d_params: &PySequence) -> Vec<G1D> {
 }
 
 #[pyfunction]
-pub fn construct_overlap_matrix_elements<'a>(
-    py: Python<'a>,
-    g1d_params: &'a PySequence,
-) -> &'a PyArray2<f64> {
+pub fn construct_overlap_matrix_elements<'py>(
+    py: Python<'py>,
+    g1d_params: &Bound<'py, PySequence>,
+) -> Bound<'py, PyArray2<f64>> {
     let gaussians = set_up_g1d_vec(g1d_params);
     let s = gs_lib::one_dim::construct_overlap_matrix_elements(&gaussians);
 
-    s.to_pyarray(py)
+    s.into_pyarray_bound(py)
 }
 
 #[pyfunction]
-pub fn construct_multipole_moment_matrix_elements<'a>(
-    py: Python<'a>,
+pub fn construct_multipole_moment_matrix_elements<'py>(
+    py: Python<'py>,
     e: u32,
     center: f64,
-    g1d_params: &'a PySequence,
-) -> &'a PyArray2<f64> {
+    g1d_params: &Bound<'py, PySequence>,
+) -> Bound<'py, PyArray2<f64>> {
     let gaussians = set_up_g1d_vec(g1d_params);
     let s_e = gs_lib::one_dim::construct_multipole_moment_matrix_elements(
         e, center, &gaussians,
     );
 
-    s_e.to_pyarray(py)
+    s_e.into_pyarray_bound(py)
 }
 
 #[pyfunction]
-pub fn construct_kinetic_operator_matrix_elements<'a>(
-    py: Python<'a>,
-    g1d_params: &'a PySequence,
-) -> &'a PyArray2<f64> {
+pub fn construct_kinetic_operator_matrix_elements<'py>(
+    py: Python<'py>,
+    g1d_params: &Bound<'py, PySequence>,
+) -> Bound<'py, PyArray2<f64>> {
     let gaussians = set_up_g1d_vec(g1d_params);
     let t =
         gs_lib::one_dim::construct_kinetic_operator_matrix_elements(&gaussians);
 
-    t.to_pyarray(py)
+    t.into_pyarray_bound(py)
 }
 
 #[pyfunction]
-pub fn construct_differential_operator_matrix_elements<'a>(
-    py: Python<'a>,
+pub fn construct_differential_operator_matrix_elements<'py>(
+    py: Python<'py>,
     f: u32,
-    g1d_params: &'a PySequence,
-) -> &'a PyArray2<f64> {
+    g1d_params: &Bound<'py, PySequence>,
+) -> Bound<'py, PyArray2<f64>> {
     let gaussians = set_up_g1d_vec(g1d_params);
     let d_e = gs_lib::one_dim::construct_differential_operator_matrix_elements(
         f, &gaussians,
     );
 
-    d_e.to_pyarray(py)
+    d_e.into_pyarray_bound(py)
 }
 
 #[pyfunction]
-pub fn construct_diff_mm_matrix_elements<'a>(
-    py: Python<'a>,
+pub fn construct_diff_mm_matrix_elements<'py>(
+    py: Python<'py>,
     e: u32,
     f: u32,
     center: f64,
-    g1d_params: &'a PySequence,
-) -> &'a PyArray2<f64> {
+    g1d_params: &Bound<'py, PySequence>,
+) -> Bound<'py, PyArray2<f64>> {
     let gaussians = set_up_g1d_vec(g1d_params);
     let l_ef = gs_lib::one_dim::construct_diff_mm_matrix_elements(
         e, f, center, &gaussians,
     );
 
-    l_ef.to_pyarray(py)
+    l_ef.into_pyarray_bound(py)
 }
 
 #[pyfunction]
-pub fn construct_gaussian_operator_matrix_elements<'a>(
-    py: Python<'a>,
-    op_params: &'a PySequence,
-    g1d_params: &'a PySequence,
-) -> &'a PyArray2<f64> {
+pub fn construct_gaussian_operator_matrix_elements<'py>(
+    py: Python<'py>,
+    op_params: &Bound<'py, PySequence>,
+    g1d_params: &Bound<'py, PySequence>,
+) -> Bound<'py, PyArray2<f64>> {
     // We only look at a single operator at a time
     let op = set_up_g1d_vec(op_params)[0];
     let gaussians = set_up_g1d_vec(g1d_params);
@@ -97,25 +97,30 @@ pub fn construct_gaussian_operator_matrix_elements<'a>(
         &op, &gaussians,
     );
 
-    gop_k.to_pyarray(py)
+    gop_k.into_pyarray_bound(py)
 }
 
 #[pymodule]
-fn one_dim_lib(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(construct_overlap_matrix_elements))?;
-    m.add_wrapped(wrap_pyfunction!(
-        construct_multipole_moment_matrix_elements
-    ))?;
-    m.add_wrapped(wrap_pyfunction!(
-        construct_kinetic_operator_matrix_elements
-    ))?;
-    m.add_wrapped(wrap_pyfunction!(
-        construct_differential_operator_matrix_elements
-    ))?;
-    m.add_wrapped(wrap_pyfunction!(construct_diff_mm_matrix_elements))?;
-    m.add_wrapped(wrap_pyfunction!(
-        construct_gaussian_operator_matrix_elements
-    ))?;
+fn one_dim_lib<'py>(m: &Bound<'py, PyModule>) -> PyResult<()> {
+    // fn one_dim_lib(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(construct_overlap_matrix_elements, m)?)?;
+    m.add_function(wrap_pyfunction!(
+        construct_multipole_moment_matrix_elements,
+        m
+    )?)?;
+    m.add_function(wrap_pyfunction!(
+        construct_kinetic_operator_matrix_elements,
+        m
+    )?)?;
+    m.add_function(wrap_pyfunction!(
+        construct_differential_operator_matrix_elements,
+        m
+    )?)?;
+    m.add_function(wrap_pyfunction!(construct_diff_mm_matrix_elements, m)?)?;
+    m.add_function(wrap_pyfunction!(
+        construct_gaussian_operator_matrix_elements,
+        m
+    )?)?;
 
     Ok(())
 }
--- src/two_dim/interface.rs
+++ src/two_dim/interface.rs
@@ -2,11 +2,11 @@ use pyo3::prelude::*;
 use pyo3::types::PySequence;
 use pyo3::wrap_pyfunction;
 
-use numpy::{PyArray2, PyArray4, ToPyArray};
+use numpy::{IntoPyArray, PyArray2, PyArray4};
 
 use gs_lib::two_dim::G2D;
 
-fn set_up_g2d_vec(g2d_params: &PySequence) -> Vec<G2D> {
+fn set_up_g2d_vec<'py>(g2d_params: &Bound<'py, PySequence>) -> Vec<G2D> {
     let g2d_param_vec = g2d_params
         .extract::<Vec<((u32, u32), f64, (f64, f64))>>()
         .unwrap();
@@ -18,23 +18,23 @@ fn set_up_g2d_vec(g2d_params: &PySequence) -> Vec<G2D> {
 }
 
 #[pyfunction]
-pub fn construct_overlap_matrix_elements<'a>(
-    py: Python<'a>,
-    g2d_params: &'a PySequence,
-) -> &'a PyArray2<f64> {
+pub fn construct_overlap_matrix_elements<'py>(
+    py: Python<'py>,
+    g2d_params: &Bound<'py, PySequence>,
+) -> Bound<'py, PyArray2<f64>> {
     let gaussians = set_up_g2d_vec(g2d_params);
     let s = gs_lib::two_dim::construct_overlap_matrix_elements(&gaussians);
 
-    s.to_pyarray(py)
+    s.into_pyarray_bound(py)
 }
 
 #[pyfunction]
-pub fn construct_multipole_moment_matrix_elements<'a>(
-    py: Python<'a>,
-    e: &'a PySequence,
-    centers: &'a PySequence,
-    g2d_params: &'a PySequence,
-) -> &'a PyArray2<f64> {
+pub fn construct_multipole_moment_matrix_elements<'py>(
+    py: Python<'py>,
+    e: &Bound<'py, PySequence>,
+    centers: &Bound<'py, PySequence>,
+    g2d_params: &Bound<'py, PySequence>,
+) -> Bound<'py, PyArray2<f64>> {
     let gaussians = set_up_g2d_vec(g2d_params);
     let s_e = gs_lib::two_dim::construct_multipole_moment_matrix_elements(
         e.extract::<(u32, u32)>().unwrap(),
@@ -42,56 +42,56 @@ pub fn construct_multipole_moment_matrix_elements<'a>(
         &gaussians,
     );
 
-    s_e.to_pyarray(py)
+    s_e.into_pyarray_bound(py)
 }
 
 #[pyfunction]
-pub fn construct_kinetic_operator_matrix_elements<'a>(
-    py: Python<'a>,
-    g2d_params: &'a PySequence,
-) -> &'a PyArray2<f64> {
+pub fn construct_kinetic_operator_matrix_elements<'py>(
+    py: Python<'py>,
+    g2d_params: &Bound<'py, PySequence>,
+) -> Bound<'py, PyArray2<f64>> {
     let gaussians = set_up_g2d_vec(g2d_params);
     let t =
         gs_lib::two_dim::construct_kinetic_operator_matrix_elements(&gaussians);
 
-    t.to_pyarray(py)
+    t.into_pyarray_bound(py)
 }
 
 #[pyfunction]
-pub fn construct_differential_operator_matrix_elements<'a>(
-    py: Python<'a>,
-    f: &'a PySequence,
-    g2d_params: &'a PySequence,
-) -> &'a PyArray2<f64> {
+pub fn construct_differential_operator_matrix_elements<'py>(
+    py: Python<'py>,
+    f: &Bound<'py, PySequence>,
+    g2d_params: &Bound<'py, PySequence>,
+) -> Bound<'py, PyArray2<f64>> {
     let gaussians = set_up_g2d_vec(g2d_params);
     let d_e = gs_lib::two_dim::construct_differential_operator_matrix_elements(
         f.extract::<(u32, u32)>().unwrap(),
         &gaussians,
     );
 
-    d_e.to_pyarray(py)
+    d_e.into_pyarray_bound(py)
 }
 
 #[pyfunction]
-pub fn construct_angular_moment_z_matrix_elements<'a>(
-    py: Python<'a>,
-    g2d_params: &'a PySequence,
-) -> &'a PyArray2<f64> {
+pub fn construct_angular_moment_z_matrix_elements<'py>(
+    py: Python<'py>,
+    g2d_params: &Bound<'py, PySequence>,
+) -> Bound<'py, PyArray2<f64>> {
     let gaussians = set_up_g2d_vec(g2d_params);
     let l_z =
         gs_lib::two_dim::construct_angular_moment_z_matrix_elements(&gaussians);
 
-    l_z.to_pyarray(py)
+    l_z.into_pyarray_bound(py)
 }
 
 #[pyfunction]
-pub fn construct_diff_mm_matrix_elements<'a>(
-    py: Python<'a>,
-    e: &'a PySequence,
-    f: &'a PySequence,
-    centers: &'a PySequence,
-    g2d_params: &'a PySequence,
-) -> &'a PyArray2<f64> {
+pub fn construct_diff_mm_matrix_elements<'py>(
+    py: Python<'py>,
+    e: &Bound<'py, PySequence>,
+    f: &Bound<'py, PySequence>,
+    centers: &Bound<'py, PySequence>,
+    g2d_params: &Bound<'py, PySequence>,
+) -> Bound<'py, PyArray2<f64>> {
     let gaussians = set_up_g2d_vec(g2d_params);
     let l_ef = gs_lib::two_dim::construct_diff_mm_matrix_elements(
         e.extract::<(u32, u32)>().unwrap(),
@@ -100,15 +100,15 @@ pub fn construct_diff_mm_matrix_elements<'a>(
         &gaussians,
     );
 
-    l_ef.to_pyarray(py)
+    l_ef.into_pyarray_bound(py)
 }
 
 #[pyfunction]
-pub fn construct_gaussian_operator_matrix_elements<'a>(
-    py: Python<'a>,
-    op_params: &'a PySequence,
-    g2d_params: &'a PySequence,
-) -> &'a PyArray2<f64> {
+pub fn construct_gaussian_operator_matrix_elements<'py>(
+    py: Python<'py>,
+    op_params: &Bound<'py, PySequence>,
+    g2d_params: &Bound<'py, PySequence>,
+) -> Bound<'py, PyArray2<f64>> {
     // We only look at a single operator at a time
     let op = &set_up_g2d_vec(op_params)[0];
     let gaussians = set_up_g2d_vec(g2d_params);
@@ -116,15 +116,15 @@ pub fn construct_gaussian_operator_matrix_elements<'a>(
         op, &gaussians,
     );
 
-    gop_k.to_pyarray(py)
+    gop_k.into_pyarray_bound(py)
 }
 
 #[pyfunction]
-pub fn construct_coulomb_attraction_operator_matrix_elements<'a>(
-    py: Python<'a>,
-    center: &'a PySequence,
-    g2d_params: &'a PySequence,
-) -> &'a PyArray2<f64> {
+pub fn construct_coulomb_attraction_operator_matrix_elements<'py>(
+    py: Python<'py>,
+    center: &Bound<'py, PySequence>,
+    g2d_params: &Bound<'py, PySequence>,
+) -> Bound<'py, PyArray2<f64>> {
     let gaussians = set_up_g2d_vec(g2d_params);
     let v_ab =
         gs_lib::two_dim::construct_coulomb_attraction_operator_matrix_elements(
@@ -132,48 +132,55 @@ pub fn construct_coulomb_attraction_operator_matrix_elements<'a>(
             &gaussians,
         );
 
-    v_ab.to_pyarray(py)
+    v_ab.into_pyarray_bound(py)
 }
 
 #[pyfunction]
-pub fn construct_coulomb_interaction_operator_matrix_elements<'a>(
-    py: Python<'a>,
-    g2d_params: &'a PySequence,
-) -> &'a PyArray4<f64> {
+pub fn construct_coulomb_interaction_operator_matrix_elements<'py>(
+    py: Python<'py>,
+    g2d_params: &Bound<'py, PySequence>,
+) -> Bound<'py, PyArray4<f64>> {
     let gaussians = set_up_g2d_vec(g2d_params);
     let u =
         gs_lib::two_dim::construct_coulomb_interaction_operator_matrix_elements(
             &gaussians,
         );
 
-    u.to_pyarray(py)
+    u.into_pyarray_bound(py)
 }
 
 #[pymodule]
-fn two_dim_lib(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(construct_overlap_matrix_elements))?;
-    m.add_wrapped(wrap_pyfunction!(
-        construct_multipole_moment_matrix_elements
-    ))?;
-    m.add_wrapped(wrap_pyfunction!(
-        construct_kinetic_operator_matrix_elements
-    ))?;
-    m.add_wrapped(wrap_pyfunction!(
-        construct_differential_operator_matrix_elements
-    ))?;
-    m.add_wrapped(wrap_pyfunction!(
-        construct_angular_moment_z_matrix_elements
-    ))?;
-    m.add_wrapped(wrap_pyfunction!(construct_diff_mm_matrix_elements))?;
-    m.add_wrapped(wrap_pyfunction!(
-        construct_gaussian_operator_matrix_elements
-    ))?;
-    m.add_wrapped(wrap_pyfunction!(
-        construct_coulomb_attraction_operator_matrix_elements
-    ))?;
-    m.add_wrapped(wrap_pyfunction!(
-        construct_coulomb_interaction_operator_matrix_elements
-    ))?;
+fn two_dim_lib<'py>(m: &Bound<'py, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(construct_overlap_matrix_elements, m)?)?;
+    m.add_function(wrap_pyfunction!(
+        construct_multipole_moment_matrix_elements,
+        m
+    )?)?;
+    m.add_function(wrap_pyfunction!(
+        construct_kinetic_operator_matrix_elements,
+        m
+    )?)?;
+    m.add_function(wrap_pyfunction!(
+        construct_differential_operator_matrix_elements,
+        m
+    )?)?;
+    m.add_function(wrap_pyfunction!(
+        construct_angular_moment_z_matrix_elements,
+        m
+    )?)?;
+    m.add_function(wrap_pyfunction!(construct_diff_mm_matrix_elements, m)?)?;
+    m.add_function(wrap_pyfunction!(
+        construct_gaussian_operator_matrix_elements,
+        m
+    )?)?;
+    m.add_function(wrap_pyfunction!(
+        construct_coulomb_attraction_operator_matrix_elements,
+        m
+    )?)?;
+    m.add_function(wrap_pyfunction!(
+        construct_coulomb_interaction_operator_matrix_elements,
+        m
+    )?)?;
 
     Ok(())
 }
